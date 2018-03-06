00452485ba0a04ef47c03680ad47875a7c1a9d64
alozur <36931786+alozur@users.noreply.github.com>
2018-03-06 19:33:54 +0100

    Interrumpo los hilos cuando se aprieta el botón.

--- Billiards.java
+++ Billiards.java
@@ -80,7 +80,11 @@ public class Billiards extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Code is executed when stop button is pushed
-			
+			if(hilos != null) {
+				for (int i = 0; i < N_BALL; i++) {
+					hilos[i].interrupt();
+				}
+			}		
 		}
 	}
 
